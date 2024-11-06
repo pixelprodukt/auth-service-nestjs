527444bfc437596193e119e68dd0c38c5833fdd1
Adrian Semmler <adrian.semmler@wdw-elab.de>
2024-11-06 18:12:19 +0100

    changed db init

--- db/init.sql
+++ db/init.sql
@@ -4,13 +4,15 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- sample data
-INSERT INTO users (name, email, password)
+INSERT INTO users (name, email, password, created_at, updated_at)
 VALUES 
-    ('John Doe', 'john.doe@example.com', 'password123'),
-    ('Jane Smith', 'jane.smith@example.com', 'securepass456'),
-    (NULL, 'alex.jones@example.com', 'pass789'),
-    ('Emily Brown', 'emily.brown@example.com', 'mypassword101');
+    ('John Doe', 'john.doe@example.com', crypt('password123', gen_salt('bf')), NOW(), NOW()),
+    ('Jane Smith', 'jane.smith@example.com', crypt('securepass456', gen_salt('bf')), NOW(), NOW()),
+    (NULL, 'alex.jones@example.com', crypt('pass789', gen_salt('bf')), NOW(), NOW()),
+    ('Emily Brown', 'emily.brown@example.com', crypt('mypassword101', gen_salt('bf')), NOW(), NOW());
--- src/models/user.entity.ts
+++ src/models/user.entity.ts
@@ -3,6 +3,7 @@ import { Role } from '../roles/role.enum';
 export interface UserEntity {
     id: number;
     name: string;
+    email: string;
     password: string;
     roles: Role[];
     createdAt: Date;
