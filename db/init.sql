6549339751f119e1d5ee9ca50014d81f21c5770e
adrian <adriansemmler@gmx.de>
2024-11-08 17:48:53 +0100

    Add some sample data and relations to db script

--- db/init.sql
+++ db/init.sql
@@ -23,9 +23,27 @@ CREATE TABLE IF NOT EXISTS users_roles (
 );
 
 -- sample data
+-- insert roles ADMIN and USER
+INSERT INTO roles (name, description)
+VALUES
+    ('ADMIN', 'Berechtigung für einen Administrator.'),
+    ('USER', 'Berechtigung für einen Benutzer.');
+
+-- insert sample user data
 INSERT INTO users (name, email, password, created_at, updated_at)
 VALUES 
     ('John Doe', 'john.doe@example.com', crypt('password123', gen_salt('bf')), NOW(), NOW()),
     ('Jane Smith', 'jane.smith@example.com', crypt('securepass456', gen_salt('bf')), NOW(), NOW()),
     (NULL, 'alex.jones@example.com', crypt('pass789', gen_salt('bf')), NOW(), NOW()),
     ('Emily Brown', 'emily.brown@example.com', crypt('mypassword101', gen_salt('bf')), NOW(), NOW());
+
+-- connect some users to a role
+INSERT INTO users_roles (user_id, role_id)
+VALUES (
+    (SELECT id FROM users WHERE email = 'john.doe@example.com'),
+    (SELECT id FROM roles WHERE name = 'ADMIN')
+),
+(
+    (SELECT id FROM users WHERE email = 'jane.smith@example.com'),
+    (SELECT id FROM roles WHERE name = 'USER')
+);
