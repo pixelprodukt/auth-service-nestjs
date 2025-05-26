c7a537fcfde3f6db745bba87b3f24e892b61b302
adrian <adriansemmler@gmx.de>
2025-05-26 22:33:29 +0200

    added config service to app module for reading db parameters from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,18 +11,20 @@ import { RoleEntity } from './entities/role.entity';
         ConfigModule.forRoot({
             isGlobal: true
         }),
-        AuthModule, 
+        AuthModule,
         UsersModule,
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.DB_HOST,
-            port: Number(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            entities: [UserEntity, RoleEntity],
-            // synchronize: true,
-            // autoLoadEntities: true,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [UserEntity, RoleEntity]
+            })
         })
     ],
     controllers: [],
