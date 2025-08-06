22eaf12f18f8d697b9c481f05ecb43edf1762710
adrian <adriansemmler@gmx.de>
2025-08-06 23:46:22 +0200

    just removed something

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { UserEntity } from './user.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'roles' })
 export class RoleEntity {
